2795972f7bac7a199110cc71eab291ee7deaf096
b.frolikov <b.frolikov@yandex.ru>
2020-05-23 00:48:02 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
+import { Segment, Menu, Container } from 'semantic-ui-react';
 
-function App() {
+const App = () => {
+  const [activeItem, setActiveItem] = useState('home');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Segment.Group>
+        <Segment inverted textAlign='center'>
+          <Menu inverted secondary>
+            <Container>
+              <Menu.Item name='home' active={activeItem === 'home'} onClick={()=>setActiveItem('home')}>
+                Home
+              </Menu.Item>
+              <Menu.Item name='projects' active={activeItem === 'projects'} onClick={()=>setActiveItem('projects')}>
+                Projects
+              </Menu.Item>
+            </Container>
+          </Menu>
+          {/*conditional rendering here */}
+        </Segment>
+        <Segment>
+          {/*conditional rendering here */}
+        </Segment>
+      </Segment.Group>
     </div>
   );
 }
-
 export default App;
